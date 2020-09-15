9d47b8f8d10ae05224ae9d8fd015ba19dcb13472
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2020-09-15 11:19:10 +0100

    Intial prototype - cordova starter files, google maps initialised, json locations, geolocation and click marker functions implemented

---
+++ uniTap/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,169 @@
+// Custom map styles // CLARKE NEWSHAM
+const mapStyle = [
+  {
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 33
+      }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#f2e5d4"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c5dac6"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 20
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      {
+        "lightness": 20
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c5c6c6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e4d7c6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fbfaf7"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#acbcc9"
+      }
+    ]
+  }
+];
+
+
+
+// Initialise map
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: {lat: 53.559798, lng: -2.873834},
+    styles: mapStyle
+
+  });
+
+  // Load the stores GeoJSON onto the map.
+  map.data.loadGeoJson('refillLocations.json');
+
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Show the information for a store when its marker is clicked.
+  map.data.addListener('click', event => {
+
+    let name = event.feature.getProperty('name');
+    let description = event.feature.getProperty('description');
+    let hours = event.feature.getProperty('hours');
+    let phone = event.feature.getProperty('phone');
+    let position = event.feature.getGeometry().get();
+    let content = `
+      <h2>${name}</h2><p>${description}</p>
+      <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
+    `
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    infoWindow.open(map);
+  });
+
+
+  
+
+  // Need to use native mobile geolocation api for the final version app 
+
+  document.getElementById('nearMe').addEventListener('click', getCurrentPosition);
+  // Try HTML5 geolocation.
+  function getCurrentPosition(){
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+  }
+  // SEARCH BOX TO BE ADDED IN LATER ITERATIONS OF THE APP
+  // var searchBox = new google.maps.places.SearchBox(input);
+        
+}
+
+
